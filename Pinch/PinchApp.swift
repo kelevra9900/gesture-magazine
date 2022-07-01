2542bb5536d7e873e4bf5f025aa425fe971ef3c1
Roger Torres <torresroger445@gmail.com>
2022-07-01 16:54:03 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Roger Torres on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
